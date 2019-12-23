61916e0b4b82b7958c85301fcaf892340d58a115
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-12-23 12:52:25 -0800

    Add prop-types, abstract UI fragments into components

--- pages/t/[tag].js
+++ pages/t/[tag].js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import Nav from '../../components/nav'
 import Link from 'next/link'
 import Head from 'next/head'
--- pages/u/[profile].js
+++ pages/u/[profile].js
@@ -1,8 +1,86 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { withRouter } from 'next/router'
 import Head from 'next/head'
+import Link from 'next/link'
 import Nav from '../../components/nav'
+import ProfileHeader from '../../components/profile-header'
 import '../../styles/styles.sass'
+
+const Bit = props => (
+    <Link href={`/bit/${props.bitID}`}>
+        <div className="box">
+            <Link href="#">
+                <h4 className="title is-5 bit-header" style={{ marginBottom: '6px', cursor: 'pointer' }}>
+                    {props.displayName}
+                    {props.verified ? ' ' : ''}
+                    {props.verified ? <i className="fas fa-check-circle verified-check-small" title="verified."></i> : ''}
+                    &emsp;
+                    <span className="handle">@{props.handle}</span>
+                </h4>
+            </Link>
+            <p className="bit-text">{props.text}</p>
+            <p className="bit-date">{props.date}</p>
+            <div className="columns interacts is-mobile">
+                <div className="column is-3">
+                    <i className="far fa-comment-alt"></i>&nbsp;&nbsp;<span className="interacts-label">{props.numReplies || 0}</span>
+                </div>
+                <div className="column is-3">
+                    <i className="far fa-heart"></i>&nbsp;&nbsp;<span className="interacts-label">{props.numHearts || 0}</span>
+                </div>
+            </div>
+            <style jsx>{`
+                .bit-header {
+                    font-size: 18px;
+                    user-select: none;
+                }
+                .handle {
+                    font-weight: 400;
+                    user-select: none;
+                }
+                .bit-text {
+                    font-weight: 500;
+                    font-size: 18px;
+                    line-height: 24px;
+                    margin-bottom: 6px;
+                }
+                .bit-date {
+                    font-weight: 500;
+                    font-size: 13px;
+                    line-height: 17px;
+                    color: #5D5D5D;
+                    margin-bottom: 6px;
+                }
+                .interacts {
+                    font-size: 13px;
+                }
+                .interacts-label {
+                    font-size: 15px;
+                }
+                .verified-check-small {
+                    font-size: 13px;
+                    user-select: none;
+                }
+                .box:hover {
+                    background: #fbfbfb;
+                    cursor: pointer;
+                }
+        `}</style>
+        </div>
+    </Link>
+)
+
+Bit.propTypes = {
+    displayName: PropTypes.string.isRequired,
+    verified: PropTypes.bool,
+    handle: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    numReplies: PropTypes.number,
+    numHearts: PropTypes.number,
+    bitID: PropTypes.string.isRequired
+}
+
 
 class Profile extends Component {
     render() {
@@ -13,88 +91,55 @@ class Profile extends Component {
                     <link key="icons" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
                 </Head>
                 <Nav />
-                <div className="hero is-small is-primary">
-                    <div className="hero-body">
-                        <div className="columns">
-                            <div className="column">
-                                <h3 className="title displayName">
-                                    Mark Zuckerberg
-                                    &nbsp;
-                                    <i className="fas fa-check-circle verified-check" title="verified."></i>
-                                    &emsp;
-                                    <span className="handle">@{this.props.router.query.profile}</span>
-                                </h3>
-                            </div>
-                            <div className="column has-text-right bit-count">
-                                <span style={{ fontSize: '20px', fontWeight: 'bold', letterSpacing: '3.5px', userSelect: 'none', textTransform: 'uppercase', userSelect: 'none' }}>4,284 bits</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <ProfileHeader
+                    displayName="Mark Zuckerberg"
+                    handle="zuck"
+                    numBits={3}
+                    verified
+                />
                 <section className="section" style={{ paddingTop: '2rem' }}>
                     <div className="columns">
                         <div className="column is-6 bit-list">
-                            <div className="box">
-                                <h4 className="title is-5" style={{ marginBottom: '6px' }}>
-                                    Mark Zuckerberg&nbsp;<i className="fas fa-check-circle verified-check-small" title="verified."></i>
-                                    &emsp;
-                                        <span className="handle">@{this.props.router.query.profile}</span>
-                                </h4>
-                                <p className="bit-text">You know, I always wondered why the speed of light was 3.0E8 meters per second. Why that number specifically? #wondering #friday</p>
-                                <p className="bit-date">January 10, 2019 at 9:14 PM</p>
-                                <div className="columns interacts is-mobile">
-                                    <div className="column is-2">
-                                        <i className="far fa-comment-alt"></i>&nbsp;&nbsp;<span className="interacts-label">1.6k</span>
-                                    </div>
-                                    <div className="column is-2">
-                                        <i className="far fa-heart"></i>&nbsp;&nbsp;<span className="interacts-label">9.4k</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <h4 className="title is-5" style={{ marginBottom: '6px' }}>
-                                    Mark Zuckerberg&nbsp;<i className="fas fa-check-circle verified-check-small" title="verified."></i>
-                                    &emsp;
-                                        <span className="handle">@{this.props.router.query.profile}</span>
-                                </h4>
-                                <p className="bit-text">You know, I always wondered why the speed of light was 3.0E8 meters per second. Why that number specifically? #wondering #friday</p>
-                                <p className="bit-date">January 10, 2019 at 9:14 PM</p>
-                                <div className="columns interacts is-mobile">
-                                    <div className="column is-2">
-                                        <i className="far fa-comment-alt"></i>&nbsp;&nbsp;<span className="interacts-label">1.6k</span>
-                                    </div>
-                                    <div className="column is-2">
-                                        <i className="far fa-heart"></i>&nbsp;&nbsp;<span className="interacts-label">9.4k</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <h4 className="title is-5" style={{ marginBottom: '6px' }}>
-                                    Mark Zuckerberg&nbsp;<i className="fas fa-check-circle verified-check-small" title="verified."></i>
-                                    &emsp;
-                                        <span className="handle">@{this.props.router.query.profile}</span>
-                                </h4>
-                                <p className="bit-text">You know, I always wondered why the speed of light was 3.0E8 meters per second. Why that number specifically? #wondering #friday</p>
-                                <p className="bit-date">January 10, 2019 at 9:14 PM</p>
-                                <div className="columns interacts is-mobile">
-                                    <div className="column is-2">
-                                        <i className="far fa-comment-alt"></i>&nbsp;&nbsp;<span className="interacts-label">1.6k</span>
-                                    </div>
-                                    <div className="column is-2">
-                                        <i className="far fa-heart"></i>&nbsp;&nbsp;<span className="interacts-label">9.4k</span>
-                                    </div>
-                                </div>
-                            </div>
+                            <Bit
+                                displayName="Mark Zuckerberg"
+                                handle={this.props.router.query.profile}
+                                text="You know, I always wondered why the speed of light was 3.0E8 meters per second. Why that number specifically? #wondering #friday"
+                                date="12h"
+                                numHearts="10"
+                                numReplies="1"
+                                bitID="128289143893498349"
+                                verified
+                            />
+                            <Bit
+                                displayName="Mark Zuckerberg"
+                                handle={this.props.router.query.profile}
+                                text="Am I actually a #robot? Had fun watching the finale of @MrRobotUSA, great show."
+                                date="18h"
+                                numHearts="100"
+                                numReplies="38"
+                                bitID="128289143893498349"
+                                verified
+                            />
+                            <Bit
+                                displayName="Mark Zuckerberg"
+                                handle={this.props.router.query.profile}
+                                text="Hello #kilobit! This is an awesome new platform where I can be myself, a robot. #totallynotrobots"
+                                date="December 22, 2019 at 8:14 AM"
+                                numHearts="2.2k"
+                                numReplies="1.0k"
+                                bitID="128289143893498349"
+                                verified
+                            />
                         </div>
                         <div className="column is-hidden-mobile"></div>
                         <div className="column is-3 is-hidden-mobile">
-                            <table className="table" style={{width: '100%'}}>
+                            <table className="table" style={{ width: '100%' }}>
                                 <tbody>
                                     <tr>
                                         <td>Joined December 22, 2019</td>
                                     </tr>
                                     <tr>
-                                        <td>hi</td>
+                                        <td>more bitstats to come...</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -102,59 +147,14 @@ class Profile extends Component {
                     </div>
                 </section>
                 <style jsx>{`
-                    .displayName {
-                        font-size: 24px;
-                        font-weight: 500;
-                        user-select: none;
-                    }
-                    .handle {
-                        font-weight: 400;
-                        user-select: none;
-                    }
-                    .verified-check {
-                        font-size: 17px;
-                        user-select: none;
-                    }
-                    .verified-check-small {
-                        font-size: 13px;
-                        user-select: none;
-                    }
-                    .verified-check:hover {
-                        color: #efefef;
-                    }
                     .bit-list {
                         padding: 0;
                         margin-left: 20px;
-                    }
-                    .bit-list > .box > .title {
-                        font-size: 18px;
-                    }
-                    .bit-text {
-                        font-weight: 500;
-                        font-size: 18px;
-                        line-height: 24px;
-                        margin-bottom: 6px;
-                    }
-                    .bit-date {
-                        font-weight: 500;
-                        font-size: 13px;
-                        line-height: 17px;
-                        color: #5D5D5D;
-                        margin-bottom: 6px;
-                    }
-                    .interacts {
-                        font-size: 13px;
-                    }
-                    .interacts-label {
-                        font-size: 15px;
                     }
                     td {
                         text-align: center !important;
                     }
                     @media screen and (max-width: 768px) {
-                        .bit-count {
-                            text-align: left !important;
-                        }
                         .bit-list {
                             margin-left: 0px;
                         }
