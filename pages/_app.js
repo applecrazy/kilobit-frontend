26d3e6a6c39f8b1be9d12236b97f12df80fc4afb
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-12-26 21:10:39 -0800

    Lint _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import App from 'next/app'
-import Link from 'next/link'
 import NProgress from 'nprogress'
 import Router from 'next/router'
 import Head from 'next/head'
@@ -8,22 +7,22 @@ import Head from 'next/head'
 NProgress.configure({ showSpinner: false })
 
 Router.events.on('routeChangeStart', url => {
-    console.log(`Loading: ${url}`)
-    NProgress.start()
+	console.log(`Loading: ${url}`)
+	NProgress.start()
 })
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 export default class KilobitApp extends App {
-    render() {
-        const { Component, pageProps } = this.props
-        return (
-            <>
-                <Head>
-                    <link rel="stylesheet" href="/nprogress.css" />
-                </Head>
-                <Component {...pageProps} />
-            </>
-        )
-    }
+	render() {
+		const { Component, pageProps } = this.props
+		return (
+			<>
+				<Head>
+					<link rel="stylesheet" href="/nprogress.css" />
+				</Head>
+				<Component {...pageProps} />
+			</>
+		)
+	}
 }
