538fc3b169c0b371d723277e3fec477ef4048d6f
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-12-27 23:53:05 -0800

    Create feed.js

---
+++ pages/feed.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+
+const Feed = () => (
+	<Layout withIcons withNavbar>
+		<section className="section">
+            TODO: build this out
+		</section>
+	</Layout>
+)
+
+
+export default Feed
