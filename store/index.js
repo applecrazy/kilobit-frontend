ecb56a1a60cd803c20c8223889923c185be1bc65
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-12-31 16:15:35 -0800

    Remove logger middleware
    We already have the redux devtools middleware.

--- store/index.js
+++ store/index.js
@@ -1,17 +1,14 @@
 import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware, compose } from 'redux'
 
 import rootReducer from '../reducers'
 
-const loggerMiddleware = createLogger()
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 const store = createStore(
 	rootReducer,
 	composeEnhancers(
 		applyMiddleware(
 			thunkMiddleware, // lets us dispatch() functions
-			loggerMiddleware // neat middleware that logs actions
 		)
 	)
 )
